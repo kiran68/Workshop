47c465bafa8a8e258e9b72055c6b6fe4ae1d6816
unknown <kiranurshil88@gmail.com>
2023-03-07 21:03:20 +0530

    Add: print the exact number

--- printNumber.sh
+++ printNumber.sh
@@ -1,6 +1,7 @@
 echo "welcome TO Work Shop"
 
-for i in {1..10}; 
-do
-    echo $i;
+for i in {1..10}; do
+  if [ $i -eq 1 ] || [ $i -eq 2 ] || [ $i -eq 4 ] || [ $i -eq 7 ] || [ $i -eq 9 ] || [ $i -eq 10 ]; then
+    echo $i
+  fi
 done
